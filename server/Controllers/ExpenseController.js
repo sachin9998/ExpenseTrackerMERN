c9e53dc9fe18a60b2cf1fc0b8a85acfca1cda151
Sachin Alam <sachinalam4216@gmail.com>
2025-01-22 15:36:00 +0530

    fetch expense added

--- server/Controllers/ExpenseController.js
+++ server/Controllers/ExpenseController.js
@@ -1,4 +1,6 @@
-export const addExpenses = (req, res) => {
+import { User } from "../Models/User.model.js";
+
+export const addExpenses = async (req, res) => {
   const { text, amount } = req.body;
   console.log(req.body);
 
@@ -6,10 +8,52 @@ export const addExpenses = (req, res) => {
     return res.status(400).json({ msg: "Please enter all fields" });
   }
 
-  const { userId, email } = req.user;
-  console.log(req.user.userId);
+  const { userId } = req.user;
+  console.log(userId);
+
+  try {
+    const userData = await User.findByIdAndUpdate(
+      userId,
+      {
+        $push: {
+          expenses: {
+            text,
+            amount,
+          },
+        },
+      },
+      { new: true }
+    );
+
+    return res.status(200).json({
+      message: "Expense added successfully",
+      success: true,
+      data: userData?.expenses,
+    });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ error: err, message: "Something went wrong", success: false });
+  }
 };
 
-export const fetchExpenses = (req, res) => {};
-export const deleteExpenses = (req, res) => {};
+export const fetchExpenses = async (req, res) => {
+  const { userId } = req.user;
+  // console.log(userId, req.body);
 
+  try {
+    const userData = await User.findById(userId).select("expenses");
+    res.status(200).json({
+      message: "Fetched Expenses successfully",
+      success: true,
+      data: userData?.expenses,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: "Something went wrong",
+      error: err,
+      success: false,
+    });
+  }
+};
+export const deleteExpenses = (req, res) => {};
--- server/Middlewares/Auth.js
+++ server/Middlewares/Auth.js
@@ -13,7 +13,7 @@ export const verifyJWT = async (req, res, next) => {
     const decodedData = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decodedData;
 
-    // console.log("decodedData", decodedData);
+    console.log("decodedData", decodedData);
 
     next();
   } catch (error) {
